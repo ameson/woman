ef62d7d6fba3abe49c4d2e015c0b4c7431440901
ameson <njsevent@gmail.com>
2024-12-07 22:39:57 +0800

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function() {
+    console.log('App Launch')
+    this.initBaiduAnalytics()
+  },
+  onShow: function() {
+    console.log('App Show')
+    this.initBaiduAnalytics()
+  },
+  onHide: function() {
+    console.log('App Hide')
+  },
+  methods: {
+    initBaiduAnalytics() {
+      // 确保在客户端环境中运行
+      if (typeof window !== 'undefined') {
+        window._hmt = window._hmt || [];
+        const hm = document.createElement('script');
+        hm.src = 'https://hm.baidu.com/hm.js?a2e1dda791e37f17ddffc039aca159ec';
+        const s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+      }
+    }
+  }
+}
 </script>
 
 <style>
-	/*每个页面公共css */
+  /*每个页面公共css */
 </style>
